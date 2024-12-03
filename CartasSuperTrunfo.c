93c72c7ddd10f28d88da131d30d5018a9190a32c
ElizabeteBeatriz <elizabetebeatriz@yahoo.com.br>
2024-12-03 16:51:32 +0000

    criei o print f

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,8 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+    printf("Desafio cartas!\n");
+    }
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
